8988fa4dce2ca77fa31e6fe81abb74219c8d31c9
SantoshKumar Avaji <santoshkumar.avaji@symphonyteleca.com>
2014-10-07 17:32:14 +0530

    Merging of root-easysetup=>root_VS2010 Merging of VB/Sample Folder

--- root_VS2010/programs/VB/Samples/WS_sample/WSServer_sample/Dao/DaoShippers.vb
+++ root_VS2010/programs/VB/Samples/WS_sample/WSServer_sample/Dao/DaoShippers.vb
@@ -359,15 +359,15 @@ Namespace WSServer_sample.Dao '衝突するため追加
 	''' <param name="fileName">ファイル名</param>
 	''' <param name="sqlUtil">SQLユーティリティ</param>
 	''' <returns>生成した静的SQL</returns>
-        Public Overloads Function ExecGenerateSQL(fileName As String, sqlUtil As SQLUtility) As String
-            ' ファイルからSQLを設定する。
-            Me.SetSqlByFile2(fileName)
+	Public Overloads Function ExecGenerateSQL(fileName As String, sqlUtil As SQLUtility) As String
+		' ファイルからSQLを設定する。
+		Me.SetSqlByFile2(fileName)
 
-            ' パラメタの設定
-            Me.SetParametersFromHt()
+		' パラメタの設定
+		Me.SetParametersFromHt()
 
-            Return MyBase.ExecGenerateSQL(sqlUtil)
-        End Function
+		Return MyBase.ExecGenerateSQL(sqlUtil)
+	End Function
 
 	#End Region
 
