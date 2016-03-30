169098638ac8db49ff28d69c644217d650df0f3f
Sandeep <sandeep.nayak077@gmail.com>
2016-03-30 20:37:01 +0530

    Merged code changes from root_VS2013 to root_VS2015 for Samples folder.

--- root_VS2015/programs/C#/Samples/WinStore_samples/WinStore_sample/Service References/SbServiceReference/Reference.cs
+++ root_VS2015/programs/C#/Samples/WinStore_samples/WinStore_sample/Service References/SbServiceReference/Reference.cs
@@ -1008,7 +1008,7 @@ namespace WinStore_sample.SbServiceReference {
         
         private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration) {
             if ((endpointConfiguration == EndpointConfiguration.ServiceForSbSoap)) {
-                return new System.ServiceModel.EndpointAddress("http://localhost:9996/WinStore_sample/WebService/ServiceForSb.asmx");
+                return new System.ServiceModel.EndpointAddress(App.BaseUrl + "WebService/ServiceForSb.asmx");
             }
             throw new System.InvalidOperationException(string.Format("Could not find endpoint with name \'{0}\'.", endpointConfiguration));
         }
