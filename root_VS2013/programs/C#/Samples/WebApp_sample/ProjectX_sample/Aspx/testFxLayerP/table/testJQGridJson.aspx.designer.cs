544a17647534d9d06ead52a3ec7a4effdb6ee8ea
supragyan panda <supragyan.panda@symphonyteleca.com>
2016-08-30 13:25:07 +0530

    Removed unwanted attribute in ProductsConditionalSearch.aspx page

--- root_VS2013/programs/C#/Samples/WebApp_sample/ProjectX_sample/Aspx/testFxLayerP/table/testJQGridJson.aspx.designer.cs
+++ root_VS2013/programs/C#/Samples/WebApp_sample/ProjectX_sample/Aspx/testFxLayerP/table/testJQGridJson.aspx.designer.cs
@@ -7,16 +7,18 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-
-public partial class Aspx_testFxLayerP_table_testJQGridJson {
+namespace ProjectX_sample.Aspx.testFxLayerP.table {
     
-    /// <summary>
-    /// form1 control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    
+    public partial class testJQGridJson {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
 }
