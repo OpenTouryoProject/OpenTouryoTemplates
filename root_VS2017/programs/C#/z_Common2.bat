a6a67509e968f688f7c68cc341ecb72dec538522
daisukenishino <daisuke.nishino.zg@hitachi-solutions.com>
2017-09-27 12:55:28 +0900

    Enhancement of build system for VS2017.

--- root_VS2017/programs/C#/z_Common2.bat
+++ root_VS2017/programs/C#/z_Common2.bat
@@ -10,14 +10,21 @@ reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" 
 @rem --------------------------------------------------
 @rem Specifying Build tool.
 @rem --------------------------------------------------
-set BUILDFILEPATH="%PROGRAM_FILES%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
-
 set BUILDFILEPATH2.0="%PROGRAM_FILES%\Microsoft Visual Studio 8\Common7\IDE\devenv.com"
 set BUILDFILEPATH3.5="%PROGRAM_FILES%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com"
 set BUILDFILEPATH4.0="%PROGRAM_FILES%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com"
 set BUILDFILEPATH4.5="%PROGRAM_FILES%\Microsoft Visual Studio 11.0\Common7\IDE\devenv.com"
 set BUILDFILEPATH4.5.1="%PROGRAM_FILES%\Microsoft Visual Studio 12.0\Common7\IDE\IDE\devenv.com"
 set BUILDFILEPATH4.6="%PROGRAM_FILES%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
+
+@rem --------------------------------------------------
+@rem VS2017 or later
+@rem --------------------------------------------------
+set CMD="..\vswhere.exe -version 15 -property installationPath"
+set BUILDFILEPATH4.7=
+for /f "usebackq delims=" %%a in (`%CMD%`) do set BUILDFILEPATH4.7=%%a
+set BUILDFILEPATH4.7="%BUILDFILEPATH4.7%\Common7\IDE\devenv.com"
+set BUILDFILEPATH=%BUILDFILEPATH4.7%
 
 @echo --------------------------------------------------
 @echo The choice of build configuration (Debug / Release).
