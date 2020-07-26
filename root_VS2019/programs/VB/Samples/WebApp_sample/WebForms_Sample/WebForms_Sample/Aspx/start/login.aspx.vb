ff2f020af753714a98c8c2670275c23e49d5f9b3
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2020-07-26 19:06:55 +0900

    Merged the changes on the body side.

--- root_VS2019/programs/VB/Samples/WebApp_sample/WebForms_Sample/WebForms_Sample/Aspx/start/login.aspx.vb
+++ root_VS2019/programs/VB/Samples/WebApp_sample/WebForms_Sample/WebForms_Sample/Aspx/start/login.aspx.vb
@@ -22,6 +22,7 @@ Imports Touryo.Infrastructure.Business.Util
 Imports Touryo.Infrastructure.Framework.Presentation
 Imports Touryo.Infrastructure.Framework.Authentication
 Imports Touryo.Infrastructure.Framework.Util
+Imports Touryo.Infrastructure.Public.Str
 Imports Touryo.Infrastructure.Public.Security.Pwd
 
 Namespace Aspx.Start
@@ -128,12 +129,13 @@ Namespace Aspx.Start
         ''' <param name="fxEventArgs">Event Handlerの共通引数</param>
         ''' <returns>URL</returns>
         Protected Function UOC_btnButton2_Click(fxEventArgs As FxEventArgs) As String
-            Return "https://localhost:44300/MultiPurposeAuthSite/Account/OAuth2Authorize" _
+            Return "https://localhost:44300/MultiPurposeAuthSite/authorize" _
                 & "?client_id=" & OAuth2AndOIDCParams.ClientID _
                 & "&response_type=code" _
                 & "&scope=profile%20email%20phone%20address%20roles%20openid" _
                 & "&state=" & Me.State _
                 & "&nonce=" & Me.Nonce _
+                & "&redirect_uri=" + CustomEncode.UrlEncode("http://localhost:9999/WebForms_Sample/Aspx/OAuth2/OAuth2AuthorizationCodeGrantClient.aspx") _
                 & "&prompt=none"
         End Function
 
