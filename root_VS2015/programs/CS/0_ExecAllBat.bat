7ace643a303e5bf598b828884cca83194c1da77c
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-03-25 15:12:00 +0900

    Resolving build errors.

--- root_VS2015/programs/CS/0_ExecAllBat.bat
+++ root_VS2015/programs/CS/0_ExecAllBat.bat
@@ -12,14 +12,11 @@ timeout 5
 
 echo | call 4_Build_Framework_Tool.bat
 echo | call 5_Build_Bat_sample.bat
-echo | call 5_Build_BatCore_sample.bat
 echo | call 5_Build_2CS_sample.bat
 echo | call 6_Build_WSSrv_sample.bat
-echo | call 6_Build_WSSrvCore_sample.bat
 echo | call 7_Build_Framework_WS.bat
 echo | call 8_Build_WSClnt_sample.bat
 echo | call 10_Build_WebApp_sample.bat
-echo | call 10_Build_WebAppCore_sample.bat
 
 @echo on
 timeout 5
