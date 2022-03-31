c16f8a874a7938dc098357b2aa6412bcf2e4a408
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2022-03-31 17:25:45 +0900

    Now, I am fixing #110.

--- z_CICD.bat
+++ z_CICD.bat
@@ -1,18 +1,5 @@
 set CURRENTDIR=%cd%
 
-rem VS2015
-cd "root_VS2015\programs\CS"
-echo | call 0_ExecAllBat.bat > ..\..\..\Build_2015CS.log
-
-cd %CURRENTDIR%
-move /Y *.log .\BackupCICDLog
-
-cd "root_VS2015\programs\VB"
-echo | call 0_ExecAllBat.bat > ..\..\..\Build_2015VB.log
-
-cd %CURRENTDIR%
-move /Y *.log .\BackupCICDLog
-
 rem VS2017
 cd "root_VS2017\programs\CS"
 echo | call 0_ExecAllBat.bat > ..\..\..\Build_2017CS.log
@@ -35,6 +22,19 @@ move /Y *.log .\BackupCICDLog
 
 cd "root_VS2019\programs\VB"
 echo | call 0_ExecAllBat.bat > ..\..\..\Build_2019VB.log
+
+cd %CURRENTDIR%
+move /Y *.log .\BackupCICDLog
+
+rem VS2022
+cd "root_VS2022\programs\CS"
+echo | call 0_ExecAllBat.bat > ..\..\..\Build_2022CS.log
+
+cd %CURRENTDIR%
+move /Y *.log .\BackupCICDLog
+
+cd "root_VS2022\programs\VB"
+echo | call 0_ExecAllBat.bat > ..\..\..\Build_2022VB.log
 
 cd %CURRENTDIR%
 move /Y *.log .\BackupCICDLog
